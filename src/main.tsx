17d5a4c46221a7fde68b5603512b6cc4b3bd879c
tobygollanm <tobygm07@gmail.com>
2025-12-05 20:12:30 -0800

    Fix background image path to match new location in about folder

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import './index.css'
 
 // Set CSS variable for background image path so CSS can access it
-document.documentElement.style.setProperty('--bg-image-url', `url(${import.meta.env.BASE_URL}images/background.jpg)`)
+document.documentElement.style.setProperty('--bg-image-url', `url(${import.meta.env.BASE_URL}images/about/background.jpg)`)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
