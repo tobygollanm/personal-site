84b8b8e4c109155d56c25e42b2037e283bad2fe3
tobygollanm <tobygm07@gmail.com>
2025-12-05 18:51:06 -0800

    Fix: Update CSS background image path to use BASE_URL

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Set CSS variable for background image path so CSS can access it
+document.documentElement.style.setProperty('--bg-image-url', `url(${import.meta.env.BASE_URL}images/background.jpg)`)
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
