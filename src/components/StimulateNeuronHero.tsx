af39db3810aee58947337c70e4a9aca3f91d0fe8
tobygollanm <tobygm07@gmail.com>
2025-12-06 17:21:26 -0800

    Fix swipe indicator position to be 40px up from original (calc(50% + 0px))

--- src/components/StimulateNeuronHero.tsx
+++ src/components/StimulateNeuronHero.tsx
@@ -800,7 +800,7 @@ export default function StimulateNeuronHero({ onDone, onPeptideImpact, onPeptide
                 animation: 'bounce 1.5s ease-in-out infinite',
                 pointerEvents: 'none',
                 left: '50%',
-                top: 'calc(50% - 40px)',
+                top: 'calc(50% + 0px)',
                 transform: 'translateX(-50%)',
                 zIndex: 100
               }}>
